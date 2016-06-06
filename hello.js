2b6fc816dd2d573969b1053bdfd30fcab2ace03b
aayushneupane <aayush_alex@yahoo.com>
2016-06-06 12:21:11 -0700

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200);
-    response.write("hello world!");
-    response.end();
-    console.log("listening on port 8080");
-}).listen(8080);
-
