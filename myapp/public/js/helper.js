e21e10665265d2c908059bf389e7b77f707f0f3f
aayush n <aayush_Alex@yahoo.com>
2016-06-07 16:18:07 -0700

    updated UI

---
+++ myapp/public/js/helper.js
@@ -0,0 +1,8 @@
+
+/**check if a entered url is in valid url format or not */
+
+function urlcheck(){
+	console.log("helpers/urlcheck");
+	element = document.getElementById("focusedInput").value;
+
+}
